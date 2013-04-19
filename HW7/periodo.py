f3ea174a51ed7dcd9389c62a9ca14f1e41f83717
Andrea Rozo <ca.rozo2200@uniandes.edu.co>
2013-04-19 15:40:54 -0500

    Calculo de periodo y radio de equilibrio

--- HW7/periodo.py
+++ HW7/periodo.py
@@ -14,46 +14,55 @@ import pylab
 import scipy
 from scipy.fftpack import fft, fftfreq
 
-# Lectura del archivo
-c = np.loadtxt('fileOut.txt')
+if (len(sys.argv)== 2):
+        
+    # Lectura del archivo
+    c = np.loadtxt('%s'%sys.argv[1])
 
-# Tamanio del archivo
-(filas,columnas) = np.shape(c)
+    # Tamanio del archivo
+    (filas,columnas) = np.shape(c)
 
 
-#Vector del radio
-radio = (c[:,1])
+    #Vector del radio
+    radio = (c[:,1])
 
-#Valor medio de R
-valorMedio = np.sum(radio)/filas
+    #Valor medio de R
+    valorMedio = np.sum(radio)/filas
 
-#Vector centrado
-radioC = (radio-valorMedio)
+    #Vector centrado
+    radioC = (radio-valorMedio)
 
-# Transformada de Fourier
-n = filas
-dt = 1 							# 1Ks de intervalo
-fft_radio = np.fft.fft(radioC)/n 			# Transformada de Fourier normalizada
-freq = np.fft.fftfreq(n,dt)				# Vector de frecuencias
-fft_radio_shift = np.fft.fftshift(fft_radio)
-freq_shifted = np.fft.fftshift(freq)
+    # Transformada de Fourier
+    n = filas
+    dt = 1 							# 1Ks de intervalo
+    fft_radio = np.fft.fft(radioC)/n 			# Transformada de Fourier normalizada
+    freq = np.fft.fftfreq(n,dt)				# Vector de frecuencias
+    fft_radio_shift = np.fft.fftshift(fft_radio)
+    freq_shifted = np.fft.fftshift(freq)
 
-# Espectro de potencias
-f2 = abs(fft_radio_shift)
+    # Espectro de potencias
+    f2 = abs(fft_radio_shift)
 
-fig1 = plt.figure()
-pylab.plot(freq_shifted,abs(f2))
-pylab.xlabel('$algo$')
-pylab.ylabel('$algo mas$')
-pylab.title('$\mathrm{Espectro\ de\ potencias\ del\ numero\ de\ manchas}$', fontsize=20)
-pylab.grid(True)
-pylab.savefig('Graph1')
+    fig1 = plt.figure()
+    pylab.plot(freq_shifted,abs(f2))
+    pylab.xlabel('$algo$')
+    pylab.ylabel('$algo mas$')
+    pylab.title('$\mathrm{Espectro\ de\ potencias\ del\ numero\ de\ manchas}$', fontsize=20)
+    pylab.grid(True)
+    pylab.savefig('Graph1')
 
-# Maximo
-m = np.where(f2 == f2.max())
-freqMax = float(freq_shifted[m[0][1]])
-per = 1/freqMax
-dias = (per*1000)/(24*3600)
+    # Periodo - Hallar el Maximo
+    m = np.where(f2 == f2.max())
+    freqMax = float(freq_shifted[m[0][1]])
+    per = 1/freqMax
+    dias = (per*1000)/(24*3600)
+    
+    # Creacion del archivo de salida
+    archivoSalida = open('period_amplitude.txt','w')
+    archivoSalida.write('Periodo: %.6f dias\n'%dias)
+    archivoSalida.write('Radio de equilibrio: %.6f cm\n'%valorMedio)
+    archivoSalida.close()
 
-# Creacion del archivo de salida
+else:
+    print "Se necesita un parametro"
 
